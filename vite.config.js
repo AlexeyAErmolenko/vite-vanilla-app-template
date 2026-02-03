9bd060bdee21d70eb9f909b5cca27854db534386
Olexii Yermolenko <AlexeyErmolenko@i.ua>
2026-02-03 11:57:43 +0200

    right init commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
@@ -35,6 +37,12 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
       emptyOutDir: true,
     },
-    plugins: [FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
   };
 });
